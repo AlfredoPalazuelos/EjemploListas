0a072a264825544400cfc98dbfb49ad05f828ba1
AlfredoPalazuelos <2002alfredo@gmail.com>
2021-12-23 10:54:35 +0100

    2 files modified

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,11 +2,18 @@
 
 import React from 'react'
 import './title.css'
+import { Header as HeaderRNE, HeaderProps, Icon } from 'react-native-elements';
 
 function Title() {
   return (
     <div className='Title'>
       <h1>Lista de Tareas ♠</h1>
+      <HeaderRNE
+        leftComponent={{
+          icon: 'menu',
+          color: '#fff',
+        }}
+        />
     </div>
   )
 }
