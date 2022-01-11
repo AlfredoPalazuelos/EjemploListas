9a64c238474a2c5eb2762fc6c440e2eeb26e988b
AlfredoPalazuelos <2002alfredo@gmail.com>
2022-01-11 12:16:01 +0100

    Menu items

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,31 @@
 
 import React from 'react'
 import {BrowserRouter as Router, Link} from 'react-router-dom';
+
+import {MenuItems} from '../data/MenuItems';
 import './barranav.css'
+
 
 class Header extends React.Component{
 constructor(props){
 super(props);
 }
 
+createMenuLink(){
 
+  const listComponents=[];
+  for(let i=0; i< MenuItems.length;i++){
+
+    listComponents.push(
+      <li className="item" key={MenuItems[i].id}><Link className="link" to ={MenuItems[i].paht}>{MenuItems[i].title}</Link></li>
+    )
+
+  }
+
+  return listComponents;
+
+
+}
 
 
 render(){
@@ -17,10 +34,7 @@ return(
   <nav className="barra">
   <h1>Mi aplicacion</h1>
   <ul className="links">
-    <li className="item"><Link className="link" to ="/">Home</Link></li>
-    <li className="item"><Link className="link" to ="/listas">Lista de tareas</Link></li>
-    <li className="item"><Link className="link" to ="/perfil">Perfil</Link></li>
-    <li className="item"><Link className="link" to ="/login">Login</Link></li>
+    {this.createMenuLink()}
   </ul>
 </nav>
 
---
+++ src/data/MenuItems.js
@@ -0,0 +1,25 @@
+import { Home } from '../components/Home';
+import { Perfil } from '../components/Perfil';
+import { Login } from '../components/Login';
+import PaginaListas from '../components/PaginaListas';
+
+export const MenuItems =[
+  {
+    paht: '/',
+    id: 1,
+    title: 'home',
+    component: Home
+  },
+  {    
+    paht: '/listas',
+    id: 2,
+    title: 'Lista de tareas',
+    component: PaginaListas
+  },
+  {    
+    paht: '/perfil',
+    id: 3,
+    title: 'Perfil',
+    component: Perfil
+  }
+]
