28f10cb16b1727a394fa69ff4ced20f84d009b63
AlfredoPalazuelos <2002alfredo@gmail.com>
2022-01-13 10:54:40 +0100

    2 files modified, 1 added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {MenuItems} from '../data/MenuItems';
-import './barranav.css';
+import './Header.css';
 
 
 class Header extends React.Component{
 constructor(props){
 super(props);
+this.state={cliked: false};
 }
 
 createMenuLink(){
@@ -17,7 +18,7 @@ createMenuLink(){
   for(let i=0; i< MenuItems.length;i++){
 
     listComponents.push(
-      <li className="item" key={MenuItems[i].id}><Link className="link" to ={MenuItems[i].paht}>{MenuItems[i].title}</Link></li>
+      <li className="navbar-link" key={MenuItems[i].id}><Link className="link" to ={MenuItems[i].paht}>{MenuItems[i].title}</Link></li>
       
     )
 
@@ -28,16 +29,26 @@ createMenuLink(){
 
 }
 
+changeClick(){
 
+  this.setState({cliked:!this.state.cliked});
+
+}
 
 
 render(){
 return(
-  <nav className="barra">
-  <h1>Mi aplicacion</h1>
-  <ul className="links">
+  <nav className="header-navbar">
+    <div className="navbar-logo">
+      <img className="logo-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGBEOcjPPVl-dKhMQLag_OaUnSQP-5JbOPiQ&usqp=CAU"></img>
+      <i className="fa-futbol" />
+    </div>
+  <ul className="navbar-menu ">
     {this.createMenuLink()}
   </ul>
+  <div className="menu-icon" onClick={ this.changeClick.bind(this)} >
+  <i  className={this.state.cliked ? "fas fa-times" : "fas fa-bars"} />
+  </div>
 </nav>
 
 
