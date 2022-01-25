2c1ee7e7c651114b3da663d606896721d7e6a0dc
AlfredoPalazuelos <2002alfredo@gmail.com>
2022-01-25 12:20:01 +0100

    localstorage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import {Container, Form, Button} from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
 
 class Home extends React.Component {
+
+  
+
   constructor(props) {
     super(props);
-    this.state={user:'',pass:''}
-    this.login=this.login.bind(this);
+    this.state = { user: '', pass: '' };
+    this.login = this.login.bind(this);
+    this.valorusuario=React.createRef();
+    this.valorpass = React.createRef();
+
   }
 
-  login(){
-    
-    this.setState({user:'Paco',pass:'1234'});
-    console.log(`${this.state.user} Has hecho click`);
+   
 
+
+
+  login() {
+    this.setState({ user: this.valorusuario.current.value, pass: this.valorpass.current.value });
+  }
+
+  componentDidMount(){
+    this.setState({user:localStorage.getItem('user'),pass:localStorage.getItem('password')});
   }
 
   render() {
-
-    if(this.state!==null && this.state.user!== null && this.state.user!== ''){ 
-    return(
-      <div className="main-site">
-      <h1>Bienvenido {this.state.user}</h1>
-    </div>
-    );
-
-    }else{
-
+    if (
+      this.state !== null &&
+      this.state.user !== null &&
+      this.state.user !== ''
+    ) {
       return (
-      
+        <div className="main-site">
+          <h1>Bienvenido {this.state.user}</h1>
+        </div>
+      );
+    } else {
+      return (
         <Container>
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Nombre de usuario o email: </Form.Label>
-              <Form.Control type="email" placeholder="Usuario o email" />
+              <Form.Control ref={this.valorusuario} type="email" placeholder="Usuario o email" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Contraseña: </Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
+              <Form.Control ref={this.valorpass} type="password" placeholder="Contraseña" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Recordarme" />
@@ -46,13 +58,17 @@ class Home extends React.Component {
             </Button>
           </Form>
         </Container>
-      
-    );
-      
+      );
     }
+  }
 
+  componetWillUnmount(){
+
+    localStorage.setItem('user',this.state.user);
+    localStorage.setItem('password',this.state.pass);
 
   }
+
 }
 
 export default Home;
